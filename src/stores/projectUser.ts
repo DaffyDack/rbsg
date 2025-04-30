b8255a71c4238d9bf71a2c7feea393d774a6ace4
Дима Мадрид <dimamadrid@MacBook-Air-Dima.local>
2025-04-30 15:21:51 +0300

    avatars

--- src/stores/projectUser.ts
+++ src/stores/projectUser.ts
@@ -3,19 +3,19 @@ import { defineStore } from 'pinia'
 
 export const useProjectsStore = defineStore('projects', () => {
   const projectsList = ref([
-    { page: 'projects', id: 1, nameProject: 'Создать рабочий стол' },
-    { page: 'projects', id: 2, nameProject: 'Собрать мангал' },
-    { page: 'projects', id: 3, nameProject: 'Настроить комп' },
-    { page: 'projects', id: 4, nameProject: 'Настроить программы' },
-    { page: 'projects', id: 5, nameProject: 'Сделка' },
-    { page: 'projects', id: 6, nameProject: 'Лид' },
-    { page: 'projects', id: 7, nameProject: 'Крафтер' },
-    { page: 'projects', id: 8, nameProject: 'Крафтер' },
+    { data: '02.10.2027', page: 'projects', id: 1, nameProject: 'Создать рабочий стол' },
+    { data: '12.10.2027', page: 'projects', id: 2, nameProject: 'Собрать мангал' },
+    { data: '20.10.2027', page: 'projects', id: 3, nameProject: 'Настроить комп' },
+    { data: '16.10.2027', page: 'projects', id: 4, nameProject: 'Настроить программы' },
+    { data: '12.10.2027', page: 'projects', id: 5, nameProject: 'Сделка' },
+    { data: '02.01.2027', page: 'projects', id: 6, nameProject: 'Лид' },
+    { data: '02.05.2027', page: 'projects', id: 7, nameProject: 'Крафтер' },
+    { data: '02.11.2027', page: 'projects', id: 8, nameProject: 'Крафтер' },
   ])
 
   function addedProjectStore (e:any) {
     let a = projectsList.value.length + 1
-    projectsList.value.push({ page: 'projects', id: a, nameProject: e }) 
+    projectsList.value.push({data: '02.11.2026', page: 'projects', id: a, nameProject: e }) 
   }
 
 
--- src/views/MyProjects.vue
+++ src/views/MyProjects.vue
@@ -5,24 +5,15 @@ import { RouterLink } from 'vue-router'
 import InputText from 'primevue/inputtext';
 import FloatLabel from 'primevue/floatlabel';
 import Button from 'primevue/button';
+import Avatar from 'primevue/avatar';
+import AvatarGroup from 'primevue/avatargroup';
+
 import { useProjectsStore } from '../stores/projectUser'
 
 const store = useProjectsStore()
 
 const name_project = ref<string>('')
-
-
 const activeIndex = ref(null)
-const items = ref([
-  { page: 'projects', id: 1, nameProject: 'Создать рабочий стол' },
-  { page: 'projects', id: 2, nameProject: 'Собрать мангал' },
-  { page: 'projects', id: 3, nameProject: 'Настроить комп' },
-  { page: 'projects', id: 4, nameProject: 'Настроить программы' },
-  { page: 'projects', id: 5, nameProject: 'Сделка' },
-  { page: 'projects', id: 6, nameProject: 'Лид' },
-  { page: 'projects', id: 7, nameProject: 'Крафтер' },
-  { page: 'projects', id: 7, nameProject: 'Крафтер' },
-])
 
 
 
@@ -56,12 +47,12 @@ function addProject() {
 <template>
   <div class="sidebar">
     <div class="card flex justify-content-center">
-        <FloatLabel class="w-[100%]">
-            <InputText class="w-[100%] mt-5 mb-5" size="small" id="username" v-model="name_project" />
-            <label for="username">Тип проекта</label>
-        </FloatLabel>
+      <FloatLabel class="w-[100%]">
+        <InputText class="w-[100%]" size="small" id="username" v-model="name_project" />
+        <label for="username">Тип проекта</label>
+      </FloatLabel>
     </div>
-    <div id="leftside-navigation" class="nano">
+    <div id="leftside-navigation" class="nano mt-5">
       <ul class="nano-content">
 
         <li class="sub-menu rounded-[10px]" v-for="(item, index) in filteredList" :key="index"
@@ -71,13 +62,39 @@ function addProject() {
             <RouterLink :to="{ name: item.page, params: { id: item.id } }">
               <i class="pi pi-file-edit unwrap"></i>
             </RouterLink>
-            <div class="link"><span>{{ item.nameProject }}</span></div>
+            <div class="link">
+              <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/asiyajavayant.png" class="mr-2"
+                shape="circle" />
+              {{ item.nameProject }}
+            </div>
+            <div class="link">
+              <AvatarGroup>
+                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/asiyajavayant.png" shape="circle" />
+                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/onyamalimba.png" shape="circle" />
+                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/ionibowcher.png" shape="circle" />
+                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/xuxuefeng.png" shape="circle" />
+                <Avatar label="+2" shape="circle" />
+              </AvatarGroup>
+              <div class="ml-4">Завершить до: {{ item.data }}</div>
+
+            </div>
           </div>
           <ul>
-            <li><a href="tables-basic.html" class="link">Basic Tables</a>
-            </li>
-
-            <li><a href="tables-data.html" class="link">Data Tables</a>
+            <li class="p-5">
+              <div class="flex">
+                <div class="w-[15%] flex-none percentCompleted">
+                  <div class="text-[20px] w-[80%] bg-black">Выполнено</div>
+                  <div class="text-[58px] w-[80%] bg-black">15%</div>
+                </div>
+                <div class="w-[70%] flex-1 ">02</div>
+                <div class="w-[15%] flex justify-center content-center items-center">
+                  <span>
+                    <div class="w-[80%] text-center text-[#fff] bg-[#6ada11] p-[10px] rounded-[10px]">Проект завершен</div>
+                  </span>
+                </div>
+              </div>
+              <div class="logs">Logs</div>
             </li>
           </ul>
         </li>
@@ -111,7 +128,7 @@ function addProject() {
   margin-right: 40px;
 
   & .sub-menu {
-    background: #293949;
+    background: #293949a1;
     margin-bottom: 10px;
   }
 
@@ -128,6 +145,27 @@ function addProject() {
         list-style-type: none;
         border-bottom: 1px solid rgba(255, 255, 255, .05);
 
+        & .percentCompleted {
+          color: #fff;
+          display: flex;
+          align-items: stretch;
+          flex-direction: column;
+          padding: 5px;
+
+          &>div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            &:nth-child(1) {
+              border-radius: 15px 15px 0 0;
+              padding-top: 8px;
+            }
+            &:nth-child(2) {
+              border-radius:  0 0 15px 15px;
+            }
+          }
+        }
+
         &.active {
           &>a {
             color: #1abc9c;
@@ -142,11 +180,13 @@ function addProject() {
           color: #aeb2b7;
           text-decoration: none;
           display: block;
-          padding: 18px 0 18px 25px;
+          padding: 10px 25px 10px 25px;
           font-size: 12px;
           outline: 0;
-          width: 100%;
           transition: all 200ms ease-in;
+          display: flex;
+          align-content: center;
+          align-items: center;
 
           &:hover {
             color: #1abc9c;
@@ -154,12 +194,14 @@ function addProject() {
 
           span {
             display: inline-block;
+
             &:hover {
               cursor: default;
             }
           }
 
           i {
+
             .fa-angle-left,
             .fa-angle-right {
               padding-top: 3px;
@@ -173,7 +215,6 @@ function addProject() {
       display: none;
 
       li {
-        background: #23313f;
         margin-bottom: 0;
         margin-left: 0;
         margin-right: 0;
